cb9f18955d4258f8c1b233a4a537ba1f5937dcec
reuben <reuben.eggar@me.com>
2016-07-27 13:32:25 +0100

    Day API

--- factories/runFactories.js
+++ factories/runFactories.js
@@ -1,24 +1,17 @@
-const Feature = require('./../models/Feature')
 const commit = require('./commit')
+const mergeFactoryReturns = require('./mergeFactoryReturns')
 
-module.exports = (factories, user, range) => {
-  return new Promise(resolve =>
-    resolve(factories.reduce(({p, c}, factory) => ({p, c, [factory.consumesLocation ? 'c' : 'p']: [...(factory.consumesLocation ? c : p), factory.productionLine(user, range)]}), {p: [], c: []}))
-  )
-  .then(({p, c}) =>
-    Promise.all(p)
-    .then(arrayOfFactoryReturns => commit(user, arrayOfFactoryReturns.reduce((flat, array) => [...flat, ...array], [])))
-    .then(() => Promise.all(c))
-    .then(arrayOfFactoryReturns => commit(user, arrayOfFactoryReturns.reduce((flat, array) => [...flat, ...array], [])))
-    .then(() => Feature.find(
-      {
-        user,
-        day: {
-          $gte: range.start,
-          $lte: range.end
-        }
-      }
-    ))
-    .then(features => features.map(feature => feature.clean()))
+module.exports = (factories, user, dayPromises) => {
+  const split = factories.reduce(({p, c}, factory) => ({p, c, [factory.consumesLocation ? 'c' : 'p']: [...(factory.consumesLocation ? c : p), factory.productionLine(user, dayPromises)]}), {p: [], c: []})
+
+  return Promise.all(split.p)
+  .then(mergeFactoryReturns)
+  .then(merged => commit(user, merged))
+  .then(completedP =>
+    Promise.all(split.c)
+    .then(mergeFactoryReturns)
+    .then(merged => commit(user, merged))
+    .then(completedC => [completedP, completedC])
+    .then(mergeFactoryReturns)
   )
 }
