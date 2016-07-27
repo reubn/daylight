95d9295f8d3076052f02d4a75ac28711613d37e1
reuben <reuben.eggar@me.com>
2016-07-27 13:33:42 +0100

    Fix refreshAccessToken removing user data

--- factories/moves/refreshAccessToken.js
+++ factories/moves/refreshAccessToken.js
@@ -2,11 +2,11 @@ const axios = require('axios')
 const config = require('./config')
 
 module.exports = (factory, user) => {
-  const {refreshToken} = factory.diplomat.getUserData(user)
+  const userData = factory.diplomat.getUserData(user)
 
-  return axios.post(config.refreshTokenURL(refreshToken))
-  .then(({data: {access_token: accessToken, expires_in: expiresIn, refresh_token: newRefreshToken, user_id: userID}}) => ({accessToken, expiresIn, refreshToken: newRefreshToken, userID}))
-  .then(data => factory.diplomat.saveUserData(user, data))
+  return axios.post(config.refreshTokenURL(userData.refreshToken))
+  .then(({data: {access_token: accessToken, expires_in: expiresIn, refresh_token: newRefreshToken}}) => ({accessToken, expiresIn, refreshToken: newRefreshToken}))
+  .then(newData => factory.diplomat.saveUserData(user, Object.assign({}, userData, newData)))
   .then(({factories: {moves}}) => moves)
   .catch(({data: {error}}) => new Error(error === 'invalid_grant' ? 'FactoryAuthError' : 'FactoryError'))
 }
