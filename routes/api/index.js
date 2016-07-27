b7815583221a8813e57df1ec4df2138f19708fa8
reuben <reuben.eggar@me.com>
2016-07-27 13:30:51 +0100

    Remove Testing Code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,10 @@
 const {Router} = require('express')
 const router = new Router()
-const moment = require('moment')
 
 const userRoute = require('./user')
-
-router.all('/', (req, res) => {
-  res.json({hello: true})
-})
+const dayRoute = require('./day')
 
 router.all('/user', userRoute)
-router.all('/TEST/:start/:end*?', ({user, params: {start, end=start}}, res) => require('../../factories').runFactories(user, {start: moment(start, 'YYYYMMDD'), end: moment(end, 'YYYYMMDD')})
-.then(a => res.json(a))
-.catch(a => res.json(a)))
+router.use('/day', dayRoute)
 
 module.exports = router
