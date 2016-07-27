de2da0852e20ee99dd4f1d83e946124891e08a22
reuben <reuben.eggar@me.com>
2016-07-27 13:35:28 +0100

    Processors can return errors [FutureImp]

--- factories/moves/processors/move.js
+++ factories/moves/processors/move.js
@@ -4,17 +4,16 @@ const Move = require('../models/Move')
 
 const {interpretMoveType} = require('../config')
 
-module.exports = (day, user, factory, {activities, lastUpdate}) =>
-  activities.map(({activity, startTime, endTime, trackPoints}) => new Move({
-    user,
+module.exports = (factory, day, {activities, lastUpdate}) =>
+  ({features: activities.map(({activity, startTime, endTime, trackPoints}) => new Move({
+    day,
     activity: interpretMoveType(activity),
     startTime: moment(startTime),
     endTime: moment(endTime),
-    day: moment(day, 'YYYYMMDD'),
     geo: {
       type: 'LineString',
       coordinates: trackPoints.map(({lat, lon}) => [lon, lat])
     },
     lastUpdate: moment(lastUpdate),
     factory: factory.slug
-  }))
+  }))})
--- factories/moves/processors/place.js
+++ factories/moves/processors/place.js
@@ -2,17 +2,21 @@ const moment = require('moment')
 
 const Place = require('../models/Place')
 
-module.exports = (day, user, factory, {place: {location: {lat, lon}, name, id: placeId}, startTime, endTime, lastUpdate}) => ([new Place({
-  user,
-  name,
-  placeId,
-  day: moment(day, 'YYYYMMDD'),
-  startTime: moment(startTime),
-  endTime: moment(endTime),
-  geo: {
-    type: 'Point',
-    coordinates: [lon, lat]
-  },
-  lastUpdate: moment(lastUpdate),
-  factory: factory.slug
-})])
+module.exports = (factory, day, {place: {location: {lat, lon}, name, id: placeId}, startTime, endTime, lastUpdate}) => {
+  const feature =
+      new Place({
+        day,
+        name,
+        placeId,
+        startTime: moment(startTime),
+        endTime: moment(endTime),
+        geo: {
+          type: 'Point',
+          coordinates: [lon, lat]
+        },
+        lastUpdate: moment(lastUpdate),
+        factory: factory.slug
+      })
+
+  return {features: [feature]}
+}
