e60d98f371358ed9e362a2a116e3f1e678137c19
reuben <reuben.eggar@me.com>
2016-07-29 20:54:21 +0100

    Handle Case where password or username is not provided

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,7 @@
 
 module.exports = (error, req, res, next) => {
   // Login
-  if(error.name === 'MismatchError' || error.message === 'NoExist') return res.status(401).json({sucess: false, reason: 'LoginFail'})
+  if(error.name === 'MismatchError' || error.message === 'NoExist' || error.name === 'AuthenticationError') return res.status(401).json({sucess: false, reason: 'LoginFail'})
 
   // Register
   if(error.message === 'TakenUsername') return res.status(409).json({sucess: false, reason: 'TakenUsername'})
--- passport/login.js
+++ passport/login.js
@@ -4,7 +4,6 @@ const User = require('../models/User')
 
 passport.use('login', new LocalStrategy({passReqToCallback: true},
       function(req, username, password, done){
-        // check in mongo if a user with username exists or not
         User.findOne({username: req.body.username})
         .then(user => {if(!user) throw new Error('NoExist'); return user})
         .then(user => user.comparePasswords(req.body.password))
