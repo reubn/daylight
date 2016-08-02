91798d4588c980207178d8c88212cbd14c6bb9a8
reuben <reuben.eggar@me.com>
2016-08-02 19:11:40 +0100

    Move Main App out of Components

--- front/index.js
+++ front/index.js
@@ -1,8 +1,40 @@
-import Daylight from 'Daylight'
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import {syncHistoryWithStore, routerMiddleware} from 'react-router-redux'
+import protect from './helpers/protect'
+
 import reducers from './reducers'
 import initials from './initials'
+
+import App from './components/App'
+import LoginContainer from './components/LoginContainer'
+import MapContainer from './components/MapContainer'
+
+function Daylight(){
+  this.store = createStore(
     reducers,
     initials,
+   compose(applyMiddleware(thunk), applyMiddleware(routerMiddleware(browserHistory)), window.devToolsExtension ? window.devToolsExtension() : f => f)
+ )
+  this.history = syncHistoryWithStore(browserHistory, this.store)
+
+  render(
+    <Provider store={this.store}>
+      <Router history={this.history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={() => <span> HOMEPAGE </span>} />
+          <Route path="/login" component={LoginContainer} onEnter={protect(this.store, null, '/map')} />
+          <Route path="/map(/:from(/:to))" component={MapContainer} onEnter={protect(this.store, '/')} />
+        </Route>
+      </Router>
     </Provider>, document.getElementById('app'))
+  return this
+}
 
 window.daylight = new Daylight()
