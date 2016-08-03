326369272ce282cbc44da5b85c72bf417929b3a1
reuben <reuben.eggar@me.com>
2016-08-03 14:01:36 +0100

    Rename Map for Clarity

--- front/components/Map/index.js
+++ front/components/Map/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import {} from 'leaflet/dist/leaflet.css'
-import {Map, ZoomControl} from 'react-leaflet'
+import {Map as LeafletMap, ZoomControl} from 'react-leaflet'
 
 import {map, mapContainer} from './style'
 import GradientPolyline from './GradientPolyline'
 import MapboxGlLayer from './MapboxGlLayer'
 
-const TheMap = props => (
+const Map = props => (
   <section className={mapContainer}>
-    <Map center={props.homeLocation} zoom={15} className={map} zoomControl={false}>
+    <LeafletMap center={props.homeLocation} zoom={15} className={map} zoomControl={false}>
       <ZoomControl position="bottomleft" />
       {/* <TileLayer url="https://api.mapbox.com/styles/v1/reubnn/cipu12kk1003rcxmb2uylg0no/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmV1Ym5uIiwiYSI6IkdwNWk5eXcifQ.ACZOaLvBQTPi24WU8LYUXg" /> */}
       <MapboxGlLayer accessToken="pk.eyJ1IjoicmV1Ym5uIiwiYSI6IkdwNWk5eXcifQ.ACZOaLvBQTPi24WU8LYUXg" style="mapbox://styles/reubnn/cipu12kk1003rcxmb2uylg0no" />
-    </Map>
+    </LeafletMap>
   </section>
     )
 
+export default Map
