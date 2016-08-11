884ce9acd047bab8b783b7f15679964d1169dfd9
reuben <reuben.eggar@me.com>
2016-08-11 15:17:57 +0100

    Default MAP_LOADING behaviour

--- front/store/reducers/map.js
+++ front/store/reducers/map.js
@@ -1,7 +1,7 @@
 import initialState from '../initials/map'
 
 const mapReducer = (state=initialState, action) => {
-  if(action.type === 'MAP_LOADING') return {...state, loading: action.status}
+  if(action.type === 'MAP_LOADING') return {...state, loading: action.status || true}
   return state
 }
 
