6717886570fa49af269d42508ab0e8bb52d4fa4d
reuben <reuben.eggar@me.com>
2016-08-15 12:05:52 +0100

    ES6

--- front/components/Map/Gradient.js
+++ front/components/Map/Gradient.js
@@ -1,28 +1,28 @@
-import {createClass} from 'react'
+import React from 'react'
 import {PropTypes} from 'react-leaflet'
 
-const Gradient = createClass({
-  contextTypes: {
+class Gradient extends React.Component {
+  static contextTypes = {
     map: PropTypes.map
-  },
+  }
   componentDidMount(){
     this.svgRoot = this.context.map.getPanes().overlayPane.firstChild
     this.gradientElement = this.svgRoot.appendChild(createGradient(this.props))
-  },
+  }
   componentDidUpdate(prevProps: Object){
     const {id} = this.props
     if(id !== prevProps.id){
       this.gradientElement.remove()
       this.gradientElement = this.svgRoot.appendChild(createGradient(this.props))
     }
-  },
+  }
   componentWillUnmount(){
     this.gradientElement.remove()
-  },
+  }
   render(){
     return null
   }
-})
+}
 
 export default Gradient
 
