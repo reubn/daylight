60afaff3cba99414c2c15f65e29c21739b3d5db9
reuben <reuben.eggar@me.com>
2016-08-18 17:22:23 +0100

    Marker Refactor

--- front/components/Map/PlaceIcon/index.js
+++ front/components/Map/PlaceIcon/index.js
@@ -2,14 +2,26 @@ import React from 'react'
 import {divIcon} from 'leaflet'
 import {Marker, Popup} from 'react-leaflet'
 
-import WithIcon from './WithIcon'
-import WithoutIcon from './WithoutIcon'
+import {marker, icon as iconStyle} from './style'
 
 const PlaceIcon = ({placeCategories, feature}) => {
   const cat = placeCategories[feature.cat]
+  const icon = cat.url
+  ? {className: marker,
+    html: `<div class="${iconStyle}" style="-webkit-mask-image: url(https://foursquare.com/img/categories_v2/${cat.url}_32.png);"></div>`,
+    iconSize: [36, 36],
+    popupAnchor: [0, -20]
+  }
+  : {
+    className: marker,
+    iconSize: [20, 20],
+    popupAnchor: [0, -12]
+  }
 
-  if(cat.url) return <WithIcon feature={feature} cat={cat} />
-  return <WithoutIcon feature={feature} />
+  return (
+    <Marker position={feature.geo} icon={divIcon(icon)} onMouseover={function(){this.openPopup()}} onMouseout={function(){this.closePopup()}}>
+      {feature.name ? <Popup closeButton={false}><span>{feature.name}</span></Popup> : null}
+    </Marker>)
 }
 
 
