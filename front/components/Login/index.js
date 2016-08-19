9545f5aaa50232d0e17ff59e1bd02ba30ed8f72c
reuben <reuben.eggar@me.com>
2016-08-19 12:56:18 +0100

    ESLint

--- front/components/Login/index.js
+++ front/components/Login/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class Login extends React.Component{
+export default class Login extends React.Component {
   constructor(props){
     super(props)
     this.state = {username: '', password: ''}
