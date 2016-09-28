f003b4f51c628c21405c9d46019a8502fec27ddb
reuben <reuben.eggar@me.com>
2016-09-28 15:55:29 +0100

    Lint Fix

--- routes/api/day/get.js
+++ routes/api/day/get.js
@@ -7,7 +7,7 @@ const {runFactories} = require('../../../factories')
 module.exports = ({user, params: {start, end=start}}, res) => {
   Promise.all(Day.rangeToDays({start: moment(start, 'YYYYMMDD'), end: moment(end, 'YYYYMMDD')}, user))
   // Split Complete and Incomplete
-  .then(days => days.reduce(({c, i}, day) => (day.complete? {c: [...c, day], i} : {c, i: [...i, day]}), {c: [], i: []}))
+  .then(days => days.reduce(({c, i}, day) => (day.complete ? {c: [...c, day], i} : {c, i: [...i, day]}), {c: [], i: []}))
   .then(({c: completeDays, i: incompleteDays}) => {
     const completedDayPromise = Promise.all(completeDays.map(day => day.getFeatures().then(features => ({day, features}))))
     const incompleteDayPromise = runFactories(user, incompleteDays) /* ...moved to runFactories */
