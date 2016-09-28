f6184bb485baef097572d3e2597d3200cc387bad
reuben <reuben.eggar@me.com>
2016-09-28 15:55:42 +0100

    Extract useful logic

--- routes/api/day/get.js
+++ routes/api/day/get.js
@@ -4,6 +4,8 @@ const Day = require('../../../models/Day')
 
 const {runFactories} = require('../../../factories')
 
+const prepareDaysForClient = require('../../../helpers/prepareDaysForClient')
+
 module.exports = ({user, params: {start, end=start}}, res) => {
   Promise.all(Day.rangeToDays({start: moment(start, 'YYYYMMDD'), end: moment(end, 'YYYYMMDD')}, user))
   // Split Complete and Incomplete
@@ -13,13 +15,8 @@ module.exports = ({user, params: {start, end=start}}, res) => {
     const incompleteDayPromise = runFactories(user, incompleteDays) /* ...moved to runFactories */
 
     return Promise.all([completedDayPromise, incompleteDayPromise])
-      .then(([daysFromComplete, daysFromIncomplete]) =>
-        [...daysFromComplete, ...daysFromIncomplete].map(({day, features, errors}) => {
-          const readyDay = day.clean()
-          const readyFeatures = features.map(feature => feature.clean())
-          const readyErrors = !!errors && !!errors.length ? errors.map(({message, name}) => ({reason: message||name||'Unhandled'})) : undefined
-          return {day: readyDay, features: readyFeatures, errors: readyErrors}
-        }))
+      .then(([daysFromComplete, daysFromIncomplete]) => [...daysFromComplete, ...daysFromIncomplete])
+      .then(prepareDaysForClient)
   })
   .then(a => res.json(a))
   .catch(a => console.log(a))
