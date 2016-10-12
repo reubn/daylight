7f0be50f1e6e576e377ff96a413dfef4c0180d27
reuben <reuben.eggar@me.com>
2016-10-12 21:56:31 +0100

    🐛 Stop double SRA

--- front/components/Map/init.js
+++ front/components/Map/init.js
@@ -2,7 +2,9 @@ import moment from 'moment'
 
 import Range from '../../helpers/Range'
 
-export default ({params: {from, to}, selectRange}) => {
+export default ({params: {from, to}, location: {action}, selectRange}) => {
+  if(action !== 'POP') return
+
   if(to) selectRange(new Range({start: moment(from, 'YYYYMMDD'), end: moment(to, 'YYYYMMDD')}), false)
   if(from && !to) selectRange(new Range({start: moment(from, 'YYYYMMDD')}), false)
 }
