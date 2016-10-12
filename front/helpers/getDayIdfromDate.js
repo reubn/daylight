faaf94000cbf613d905cbbe00ddb45c476c2d24c
reuben <reuben.eggar@me.com>
2016-10-12 20:42:36 +0100

    Use day not dayId

--- front/helpers/getDayIdfromDate.js
+++
@@ -1,1 +0,0 @@
-export default (getState, date) => (getState().map.cache.days.find(({day: {date: existingDate}}) => date.isSame(existingDate)) || {day: {id: null}}).day.id
--- front/store/actions/selectRange.js
+++ front/store/actions/selectRange.js
@@ -2,17 +2,17 @@ import axios from 'axios'
 import {replace} from 'react-router-redux'
 
 import Range from '../../helpers/Range'
-import getDayIdfromDate from '../../helpers/getDayIdfromDate'
+import getDayFromDate from '../../helpers/getDayFromDate'
 
 function selectRangeAction(dispatch, getState, range, redirect=true){
   console.log('SRA', redirect)
   if(redirect) dispatch(replace(`/map/${range.toURL()}`))
   dispatch({type: 'MAP_LOADING'})
 
-  const {cached: cachedDayIds, onServer: onServerDates} = range.toDates()
+  const {cached: cachedDays, onServer: onServerDates} = range.toDates()
   .reduce(({cached, onServer}, date) => {
-    const cachedDayId = getDayIdfromDate(getState, date)
-    if(cachedDayId) return {cached: [...cached, cachedDayId], onServer}
+    const cachedDay = getDayFromDate(getState, date)
+    if(cachedDay) return {cached: [...cached, cachedDay], onServer}
     return {cached, onServer: [...onServer, date]}
   }, {cached: [], onServer: []})
 
@@ -26,11 +26,10 @@ function selectRangeAction(dispatch, getState, range, redirect=true){
     })], []
   )
   Promise.all(serverPromises)
-  .then(arrayOfResponces => arrayOfResponces.reduce((flat, res) => [...flat, ...res.map(day => day.day.id)], []))
-  .then(dayIdsFromServer => (console.log(dayIdsFromServer), [...cachedDayIds, ...dayIdsFromServer]))
-  .then(dayIds => {
-    console.log('1')
-    dispatch({type: 'SELECT_RANGE', range, dayIds})
+  .then(arrayOfResponces => arrayOfResponces.reduce((flat, res) => [...flat, ...res], []))
+  .then(daysFromServer => (console.log(daysFromServer), [...cachedDays, ...daysFromServer]))
+  .then(days => {
+    dispatch({type: 'SELECT_RANGE', range, days})
     dispatch({type: 'MAP_LOADING', status: false})
   })
 }
