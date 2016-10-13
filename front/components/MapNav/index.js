0c8acaa604415a9b4f86d7305ecd43253e95218e
reuben <reuben.eggar@me.com>
2016-10-13 17:29:36 +0100

    🐛

--- front/components/MapNav/index.js
+++ front/components/MapNav/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import {navbar, arrow, start, slash, end} from './style'
+import {mapnav, arrow, start, slash, end} from './style'
 
 const MapNav = ({goYesterday, goTomorrow, selectRange, range}) => {
   const freeRange = range || {}
   const startDisplay = freeRange.start ? range.start.format('YYYYMMDD') : null
   const endDisplay = (freeRange.end && !freeRange.start.isSame(freeRange.end)) ? range.end.format('YYYYMMDD') : null
   return (
-    <span className={navbar}>
+    <span className={mapnav}>
       <span className={arrow}>&lt;-</span>
       <span className={start}>{startDisplay}</span>
       <span className={slash}>/</span>
