e12a5bb92b7e754f0eda33eed113ab2a1bcbe874
reuben <reuben.eggar@me.com>
2016-10-13 17:21:36 +0100

    MapNav v2

--- front/components/MapNav/index.js
+++ front/components/MapNav/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const MapNav = ({goYesterday, goTomorrow, selectRange, range}) => (
-  <span>
-    {range ? range.toURL() : 'Nada!'}
-  </span>
-  )
+import {navbar, arrow, start, slash, end} from './style'
+
+const MapNav = ({goYesterday, goTomorrow, selectRange, range}) => {
+  const freeRange = range || {}
+  const startDisplay = freeRange.start ? range.start.format('YYYYMMDD') : null
+  const endDisplay = (freeRange.end && !freeRange.start.isSame(freeRange.end)) ? range.end.format('YYYYMMDD') : null
+  return (
+    <span className={navbar}>
+      <span className={arrow}>&lt;-</span>
+      <span className={start}>{startDisplay}</span>
+      <span className={slash}>/</span>
+      <span className={end}>{endDisplay}</span>
+      <span className={arrow}>-&gt;</span>
+    </span>
+  )}
 
 export default MapNav
