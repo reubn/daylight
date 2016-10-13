fd734c5c76f30f43789f0942a1f89abb3503f33e
reuben <reuben.eggar@me.com>
2016-10-13 18:42:37 +0100

    🌱 Navigation Buttons

--- front/components/RangeSelector/index.js
+++ front/components/RangeSelector/index.js
@@ -8,11 +8,11 @@ const RangeSelector = ({goYesterday, goTomorrow, selectRange, range}) => {
   const endDisplay = (freeRange.end && !freeRange.start.isSame(freeRange.end)) ? range.end.format('YYYYMMDD') : null
   return (
     <span className={rangeSelector}>
-      <span className={arrow}>&lt;-</span>
+      <span className={arrow} onClick={goYesterday}>&lt;-</span>
       <span className={start}>{startDisplay}</span>
       <span className={slash}>/</span>
       <span className={end}>{endDisplay}</span>
-      <span className={arrow}>-&gt;</span>
+      <span className={arrow} onClick={goTomorrow}>-&gt;</span>
     </span>
   )}
 
