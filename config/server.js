3ea2c362f48463b71595c0de28f9a82c1b3a7af5
reuben <reuben.eggar@me.com>
2016-10-15 12:23:44 +0100

    🐛Broken Status Code Display

--- config/server.js
+++ config/server.js
@@ -6,6 +6,6 @@ module.exports = {
   methodIndicator: ({method}) => ({GET: '⛽️', POST: '📮', PUT: '📥', DELETE: '🗑', HEAD: '👨🏽', TRACE: '🔎', PATCH: '📝'}[method]),
   secureIndicator: ({secure}) => (secure ? '🔐' : '🔓'),
   pathIndicator: ({originalUrl: url}) => url.split('/').map((part, index) => colors[['magenta', 'magenta', 'blue', 'cyan', 'green', 'yellow', 'red', 'grey', 'black'][index % 9]](part)).join(colors.white('/')),
-  statusCodeIndicator: (req, {statusCode}, {0: a, 1: b, 2: c}=statusCode.toString()) => `${a}\uFE0F\u20E3 ${b}\uFE0F\u20E3 ${c}\uFE0F\u20E3`,
+  statusCodeIndicator: (req, {statusCode}) => statusCode.toString(),
   customEmojiIndicator: (req, {locals: {emoji}}) => emoji || '🔗'
 }
