c867fb8bbf8c0ea8bc573bfa73791ede87c3e349
reuben <reuben.eggar@me.com>
2016-10-16 21:30:24 +0100

    Update Button

--- front/components/MapNav/index.js
+++ front/components/MapNav/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import RangeSelectorContainer from '../RangeSelectorContainer'
+import UpdateButtonContainer from '../UpdateButtonContainer'
 
 import {mapNav} from './style'
 
 const MapNav = () => (
   <header className={mapNav}>
     <RangeSelectorContainer />
+    <UpdateButtonContainer />
   </header>
 )
 
