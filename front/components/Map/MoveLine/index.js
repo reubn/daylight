1c5e6a8dbe83c65c9f1b13bb771107ff42339f82
reuben <reuben.eggar@me.com>
2016-10-18 20:16:20 +0100

    Set Weight on Polylines

--- front/components/Map/MoveLine/index.js
+++ front/components/Map/MoveLine/index.js
@@ -7,6 +7,7 @@ const MoveLine = ({activityTypes, displayFeature}) =>
     id={displayFeature.id}
     gradient={activityTypes[displayFeature.activity].map((colour, index, array) => ({offset: (100 * index) / (array.length - 1), colour}))}
     opacity={0.9}
+    weight={5}
     positions={displayFeature.geo}
     onMouseover={function(){this.bringToFront()}}
     onMouseout={function(){this.bringToBack()}}
