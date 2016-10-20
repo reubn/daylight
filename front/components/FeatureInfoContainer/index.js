20cb77efb53816667b8c43785ed12542a607bed2
reuben <reuben.eggar@me.com>
2016-10-20 20:10:33 +0100

    Pass activityTypes

--- front/components/FeatureInfoContainer/index.js
+++ front/components/FeatureInfoContainer/index.js
@@ -2,7 +2,7 @@ import {connect} from 'react-redux'
 
 import FeatureInfo from '../FeatureInfo'
 
-const mapStateToProps = ({map: {selected: {selectedFeature}}}) => ({feature: selectedFeature})
+const mapStateToProps = ({map: {selected: {selectedFeature}}, aesthetics: {activityTypes}}) => ({feature: selectedFeature, activityTypes})
 const mapDispatchToProps = {
   close: () => ({type: 'SELECT_FEATURE', displayFeature: null})
 }
