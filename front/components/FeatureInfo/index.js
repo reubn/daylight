b07875b857de6e3a4f7a6c1551ea93ae40f09c5f
reuben <reuben.eggar@me.com>
2016-10-20 20:10:40 +0100

    Header

--- front/components/FeatureInfo/index.js
+++ front/components/FeatureInfo/index.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
-import {featureInfo, hidden} from './style'
+import {featureInfo, hidden, header} from './style'
 
-const FeatureInfo = ({feature, close}) => (
-  <section className={`${featureInfo} ${feature ? '' : hidden}`}>
-    {JSON.stringify(feature)}
-  </section>
+const FeatureInfo = ({feature, activityTypes, close}) => {
+  if(!feature) return <section className={`${featureInfo} ${hidden}`} />
+
+  const location = () => (
+    <header className={header}>
+      <span>
+        {feature.name}
+      </span>
+    </header>
+    )
+
+  const move = () => (
+    <header className={header} style={{background: `linear-gradient(300deg,${activityTypes[feature.activity].join()})`}}>
+      <span>
+        {(([f, ...r]) => f.toUpperCase()+r.join``)(feature.activity)}
+      </span>
+    </header>
+    )
+
+  return (
+    <section className={featureInfo}>
+      {feature.activity ? move() : location()}
+      {JSON.stringify(feature)}
+    </section>
   )
+}
 
 export default FeatureInfo
