59a326fbfd09fc49f5a82d9503160abb265a3bc2
reuben <reuben.eggar@me.com>
2016-10-20 16:51:28 +0100

    🙄Tag Rename

--- front/components/FeatureInfo/index.js
+++ front/components/FeatureInfo/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 import {featureInfo, hidden} from './style'
 
 const FeatureInfo = ({feature, close}) => (
-  <span className={`${featureInfo} ${feature ? '' : hidden}`}>
+  <section className={`${featureInfo} ${feature ? '' : hidden}`}>
     {JSON.stringify(feature)}
-  </span>
+  </section>
   )
 
 export default FeatureInfo
