78cc381994c4201cbd10aceaa2a434dffd0214f4
reuben <reuben.eggar@me.com>
2016-10-20 09:00:29 +0100

    Add selected styles to LocationIcon

--- front/components/Map/LocationIcon/index.js
+++ front/components/Map/LocationIcon/index.js
@@ -4,18 +4,18 @@ import {Marker} from 'react-leaflet'
 
 import FeaturePopup from '../FeaturePopup'
 
-import {marker, icon as iconStyle} from './style'
+import {marker, icon as iconStyle, selected as selectedStyle} from './style'
 
-const LocationIcon = ({locationCategories, iconImageSize, displayFeature, selectFeature}) => {
+const LocationIcon = ({locationCategories, iconImageSize, displayFeature, selected, selectFeature}) => {
   const cat = locationCategories[displayFeature.cat]
   const icon = cat.url
-  ? {className: marker,
+  ? {className: `${marker} ${selected ? selectedStyle : ''}`,
     html: `<div class="${iconStyle}" style="-webkit-mask-image: url(https://foursquare.com/img/categories_v2/${cat.url}_${iconImageSize}.png);"></div>`,
     iconSize: [36, 36],
     popupAnchor: [0, -20]
   }
   : {
-    className: marker,
+    className: `${marker} ${selected ? selectedStyle : ''}`,
     iconSize: [20, 20],
     popupAnchor: [0, -12]
   }
