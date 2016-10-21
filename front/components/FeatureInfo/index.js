0b54c4e8a3d0cd2d778058c379362512e5275816
reuben <reuben.eggar@me.com>
2016-10-21 22:56:57 +0100

    Latlng Display

--- front/components/FeatureInfo/index.js
+++ front/components/FeatureInfo/index.js
@@ -1,22 +1,29 @@
 import React from 'react'
 
-import {featureInfo, hidden, header} from './style'
+import {featureInfo, hidden, header, title, latlng} from './style'
 
 const FeatureInfo = ({feature, activityTypes, close}) => {
   if(!feature) return <section className={`${featureInfo} ${hidden}`} />
 
   const location = () => (
     <header className={header}>
-      <span>
-        {feature.name}
+      <span className={title}>
+        <span>
+          {feature.name}
+        </span>
+      </span>
+      <span className={latlng}>
+        {(({lat, lng}) => `${lat.toFixed(5)}, ${lng.toFixed(5)}`)(feature.geo)}
       </span>
     </header>
     )
 
   const move = () => (
     <header className={header} style={{background: `linear-gradient(300deg,${activityTypes[feature.activity].join()})`}}>
-      <span>
-        {(([f, ...r]) => f.toUpperCase()+r.join``)(feature.activity)}
+      <span className={title}>
+        <span>
+          {(([f, ...r]) => f.toUpperCase()+r.join``)(feature.activity)}
+        </span>
       </span>
     </header>
     )
