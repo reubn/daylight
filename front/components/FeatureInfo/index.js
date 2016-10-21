c1f32ac6f6bb651a0e8d2e67fe591698f535d997
reuben <reuben.eggar@me.com>
2016-10-21 23:49:41 +0100

    🌱Copy To Clipboard

--- front/components/FeatureInfo/index.js
+++ front/components/FeatureInfo/index.js
@@ -1,22 +1,29 @@
 import React from 'react'
 
+import CopyToClipboard from 'react-copy-to-clipboard'
+
 import {featureInfo, hidden, header, title, latlng} from './style'
 
 const FeatureInfo = ({feature, activityTypes, close}) => {
   if(!feature) return <section className={`${featureInfo} ${hidden}`} />
 
-  const location = () => (
-    <header className={header}>
-      <span className={title}>
-        <span>
-          {feature.name}
+  const location = () => {
+    const latlngFormatted = (({lat, lng}) => `${lat.toFixed(5)}, ${lng.toFixed(5)}`)(feature.geo)
+    return (
+      <header className={header}>
+        <span className={title}>
+          <span>
+            {feature.name}
+          </span>
         </span>
-      </span>
-      <span className={latlng}>
-        {(({lat, lng}) => `${lat.toFixed(5)}, ${lng.toFixed(5)}`)(feature.geo)}
-      </span>
-    </header>
+        <CopyToClipboard text={latlngFormatted}>
+          <span className={latlng}>
+            {latlngFormatted}
+          </span>
+        </CopyToClipboard>
+      </header>
     )
+  }
 
   const move = () => (
     <header className={header} style={{background: `linear-gradient(300deg,${activityTypes[feature.activity].join()})`}}>
