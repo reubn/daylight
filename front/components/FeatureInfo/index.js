65c795385f865682c4b814504ed4bf9c0ffbe924
reuben <reuben.eggar@me.com>
2016-10-23 13:21:31 +0100

    🌱Fields

--- front/components/FeatureInfo/index.js
+++ front/components/FeatureInfo/index.js
@@ -2,40 +2,56 @@ import React from 'react'
 
 import CopyToClipboard from 'react-copy-to-clipboard'
 
-import {featureInfo, hidden, header, title, latlng, close as closeStyle} from './style'
+import {featureInfo, hidden, header, title, latlng, close as closeStyle, field} from './style'
 
-const FeatureInfo = ({feature, activityTypes, close}) => {
+const FeatureInfo = ({feature, activityTypes, locationCategories, close}) => {
   if(!feature) return <section className={`${featureInfo} ${hidden}`} />
 
   const location = () => {
-    const latlngFormatted = (({lat, lng}) => `${lat.toFixed(5)}, ${lng.toFixed(5)}`)(feature.geo)
+    const latlngFormatted = `${feature.geo.lat.toFixed(5)}, ${feature.geo.lng.toFixed(5)}`
+    const cat = locationCategories[feature.cat]
     return (
-      <header className={header}>
-        <span className={closeStyle} onClick={close}>{'>'}</span>
-        <span className={title}>
-          <span>
-            {feature.name || 'Unknown'}
+      <span>
+        <header className={header}>
+          <span className={closeStyle} onClick={close}>{'>'}</span>
+          <span className={title}>
+            <span>
+              {feature.name || 'Unknown'}
+            </span>
           </span>
+          <CopyToClipboard text={latlngFormatted}>
+            <span className={latlng}>
+              {latlngFormatted}
+            </span>
+          </CopyToClipboard>
+        </header>
+        <span className={field}>
+          <label>Category</label>
+          <value>{cat.name}</value>
         </span>
-        <CopyToClipboard text={latlngFormatted}>
-          <span className={latlng}>
-            {latlngFormatted}
-          </span>
-        </CopyToClipboard>
-      </header>
+      </span>
     )
   }
 
-  const move = () => (
-    <header className={header} style={{background: `linear-gradient(300deg,${activityTypes[feature.activity].join()})`}}>
-      <span className={closeStyle} onClick={close}>{'>'}</span>
-      <span className={title}>
-        <span>
-          {(([f, ...r]) => f.toUpperCase()+r.join``)(feature.activity)}
+  const move = () => {
+    const activityName = (([f, ...r]) => f.toUpperCase()+r.join``)(feature.activity)
+    return (
+      <span>
+        <header className={header} style={{background: `linear-gradient(300deg,${activityTypes[feature.activity].join()})`}}>
+          <span className={closeStyle} onClick={close}>{'>'}</span>
+          <span className={title}>
+            <span>
+              {activityName}
+            </span>
+          </span>
+        </header>
+        <span className={field}>
+          <label>Activity</label>
+          <value>{activityName}</value>
         </span>
       </span>
-    </header>
     )
+  }
 
   return (
     <section className={featureInfo}>
--- front/components/FeatureInfoContainer/index.js
+++ front/components/FeatureInfoContainer/index.js
@@ -2,7 +2,7 @@ import {connect} from 'react-redux'
 
 import FeatureInfo from '../FeatureInfo'
 
-const mapStateToProps = ({map: {selected: {selectedFeature}}, aesthetics: {activityTypes}}) => ({feature: selectedFeature, activityTypes})
+const mapStateToProps = ({map: {selected: {selectedFeature}}, aesthetics: {activityTypes, locationCategories}}) => ({feature: selectedFeature, activityTypes, locationCategories})
 const mapDispatchToProps = {
   close: () => ({type: 'SELECT_FEATURE', displayFeature: null})
 }
