4d046c7a0e700c251778680a01528e2b9a202102
reuben <reuben.eggar@me.com>
2016-10-25 22:18:07 +0100

    Use getters on models

--- front/components/FeatureInfo/index.js
+++ front/components/FeatureInfo/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import moment from 'moment'
 
 import CopyToClipboard from 'react-copy-to-clipboard'
-import humanizeDuration from 'humanize-duration'
 
 import {featureInfo, hidden, header, title, latlng, close as closeStyle, main, field} from './style'
 
@@ -10,7 +8,6 @@ const FeatureInfo = ({feature, activityTypes, locationCategories, close}) => {
   if(!feature) return <section className={`${featureInfo} ${hidden}`} />
 
   const location = () => {
-    const latlngFormatted = `${feature.geo.lat.toFixed(5)}, ${feature.geo.lng.toFixed(5)}`
     const cat = locationCategories[feature.cat]
     return (
       <span>
@@ -21,9 +18,9 @@ const FeatureInfo = ({feature, activityTypes, locationCategories, close}) => {
               {feature.name || 'Unknown'}
             </span>
           </span>
-          <CopyToClipboard text={latlngFormatted}>
+          <CopyToClipboard text={feature.formattedLatLng}>
             <span className={latlng}>
-              {latlngFormatted}
+              {feature.formattedLatLng}
             </span>
           </CopyToClipboard>
         </header>
@@ -37,33 +34,28 @@ const FeatureInfo = ({feature, activityTypes, locationCategories, close}) => {
     )
   }
 
-  const move = () => {
-    const activityName = (([f, ...r]) => f.toUpperCase()+r.join``)(feature.activity)
-    const duration = humanizeDuration(moment.duration(moment(feature.endTime).diff(feature.startTime)))
-    console.log(moment.duration(moment(feature.endTime).diff(feature.startTime)))
-    return (
-      <span>
-        <header className={header} style={{background: `linear-gradient(300deg,${activityTypes[feature.activity].join()})`}}>
-          <span className={closeStyle} onClick={close}>{'->'}</span>
-          <span className={title}>
-            <span>
-              {activityName}
-            </span>
-          </span>
-        </header>
-        <span className={main}>
-          <span className={field}>
-            <label>Activity</label>
-            <value>{activityName}</value>
-          </span>
-          <span className={field}>
-            <label>Duration</label>
-            <value>{duration}</value>
+  const move = () => (
+    <span>
+      <header className={header} style={{background: `linear-gradient(300deg,${activityTypes[feature.activity].join()})`}}>
+        <span className={closeStyle} onClick={close}>{'->'}</span>
+        <span className={title}>
+          <span>
+            {feature.activityName}
           </span>
         </span>
+      </header>
+      <span className={main}>
+        <span className={field}>
+          <label>Activity</label>
+          <value>{feature.activityName}</value>
+        </span>
+        <span className={field}>
+          <label>Duration</label>
+          <value>{feature.duration}</value>
+        </span>
       </span>
+    </span>
     )
-  }
 
   return (
     <section className={featureInfo}>
