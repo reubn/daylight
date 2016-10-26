a058fce30d83d8f578674ccf1968cfe19121dc6a
reuben <reuben.eggar@me.com>
2016-10-26 08:40:20 +0100

    Rewrite to classes

--- front/Models/Range/index.js
+++ front/Models/Range/index.js
@@ -6,23 +6,30 @@ import datesToRanges from './datesToRanges'
 import toURL from './toURL'
 import fromURL from './fromURL'
 
-function Range({start, end=start}){
-  if(end.isBefore(start)) start = end
+class Range {
+  constructor({start, end=start}){
+    if(end.isBefore(start)) start = end
 
-  this.start = moment(start)
-  this.end = moment(end)
-
-  this.toDates = () => rangeToDates(this)
-  this.toURL = () => toURL(this)
-  this.isSameAs = range2 => {
+    this.start = moment(start)
+    this.end = moment(end)
+  }
+  toDates(){
+    return rangeToDates(this)
+  }
+  toURL(){
+    return toURL(this)
+  }
+  isSameAs(range2){
     if(!range2) return false
     if(!(range2.start && range2.end)) return false
     return (this.start.isSame(range2.start) && this.end.isSame(range2.end))
   }
-
-  return this
+  static datesToRanges(dates){
+    return datesToRanges(Range, dates)
+  }
+  static fromURL(url){
+    return fromURL(Range, url)
+  }
 }
-Range.datesToRanges = dates => datesToRanges(Range, dates)
-Range.fromURL = url => fromURL(Range, url)
 
 export default Range
