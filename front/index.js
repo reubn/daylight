ef13014293b4d339293ea3ee710936f21c804e46
reuben <reuben.eggar@me.com>
2016-10-28 13:31:08 +0100

    HTML Optims

--- front/index.js
+++ front/index.js
@@ -1,19 +1,13 @@
 module.exports = (user, activityTypes, locationCategories) =>
-
   `<!DOCTYPE html>
-  <html>
-  <head>
     <title>daylight 🌍</title>
     <meta charset="utf-8">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <meta name="apple-mobile-web-app-capable" content="yes">
-  </head>
-  <body>
     <section id="app"></section>
     <script type="text/javascript">
       window.dUD = ${JSON.stringify(user)}
-      window.dAS = ${JSON.stringify({activityTypes, locationCategories})}</script>
-    <script type="text/javascript" src="/bundle.js" charset="utf-8"></script>
-  </body>
-  </html>
-`
+      window.dAS = ${JSON.stringify({activityTypes, locationCategories})}
+    </script>
+    <script src="/bundle.js"></script>
+  </html>`
