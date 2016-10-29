0a757c6e00fdaecec10667ad38ab6daa185def53
reuben <reuben.eggar@me.com>
2016-10-29 21:59:04 +0100

    🐛🎂

--- front/components/LogoContainer/index.js
+++ front/components/LogoContainer/index.js
@@ -1,6 +1,6 @@
 import {connect} from 'react-redux'
 import Logo from '../Logo'
 
-const mapStateToProps = ({user: {birthday}}) => ({birthday: birthday.isSame(new Date(), 'd')})
+const mapStateToProps = ({user: {birthday, loggedIn}}) => ({birthday: birthday.isSame(new Date(), 'd') && loggedIn})
 
 export default connect(mapStateToProps)(Logo)
