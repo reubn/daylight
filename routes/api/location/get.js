c1593f664776f2d42ce0cc5edbcd95013c55c2ce
reuben <reuben.eggar@me.com>
2016-10-29 11:32:15 +0100

    🔒Scope Location Search to User

--- routes/api/location/get.js
+++ routes/api/location/get.js
@@ -1,7 +1,7 @@
 const Location = require('../../../models/Location')
 
 module.exports = ({user, params: {id}}, res, next) =>
-  Location.findById(id)
+  Location.findOne({_id: id, user})
   .then(location => {
     if(location) return res.json(location.clean())
     next(new Error('ResourceNotFound'))
