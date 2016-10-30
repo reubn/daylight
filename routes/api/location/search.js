e3a3024f7412507397d746f0ad54c87ee93b9af3
reuben <reuben.eggar@me.com>
2016-10-30 15:34:01 +0000

    Send cat on search

--- routes/api/location/search.js
+++ routes/api/location/search.js
@@ -2,7 +2,7 @@ const Location = require('../../../models/Location')
 
 module.exports = ({user, params: {name}}, res, next) =>
   Location.find({name: new RegExp(`^${name.trim()}`, 'i'), user})
-  .select({name: 1})
+  .select({name: 1, cat: 1})
   .then(locations => locations.map(location => location.clean()))
   .then(clean => res.json(clean))
   .catch(next)
