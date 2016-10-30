b54751eb1bec7922af6cf4dd3b3708d0223e95e5
reuben <reuben.eggar@me.com>
2016-10-30 17:19:59 +0000

    loginForm actions

--- front/store/actions/login.js
+++ front/store/actions/login.js
@@ -4,14 +4,19 @@ import {replace} from 'react-router-redux'
 import User from '../../models/User'
 
 function loginAction(dispatch, username, password){
+  dispatch({type: 'LOGINFORM_ERRORS', errors: false})
+  dispatch({type: 'LOGINFORM_LOADING'})
   axios.post('/login', {
     username, password
   })
   .then(function({data: user}){
     dispatch({type: 'USER', user: new User(user)})
     dispatch(replace('/map'))
+    dispatch({type: 'LOGINFORM_LOADING', status: false})
   })
   .catch(function(error){
+    dispatch({type: 'LOGINFORM_ERRORS'})
+    dispatch({type: 'LOGINFORM_LOADING', status: false})
     console.log(error)
   })
 }
