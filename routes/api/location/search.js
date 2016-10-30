0cccee346ae97f421871291d982702b7b719db07
reuben <reuben.eggar@me.com>
2016-10-30 16:05:33 +0000

    Escape Regex

--- routes/api/location/search.js
+++ routes/api/location/search.js
@@ -1,7 +1,9 @@
+const escapeRegExp = require('lodash.escaperegexp')
+
 const Location = require('../../../models/Location')
 
 module.exports = ({user, params: {name}}, res, next) =>
-  Location.find({name: new RegExp(`^${name.trim()}`, 'i'), user})
+  Location.find({name: new RegExp(`^${escapeRegExp(name.trim())}`, 'i'), user})
   .select({name: 1, cat: 1})
   .then(locations => locations.map(location => location.clean()))
   .then(clean => res.json(clean))
