998aa2114a1686122e0b31b0e2f72c210ac700f1
reuben <reuben.eggar@me.com>
2016-11-12 11:05:38 +0000

    Start with yesterday

--- front/components/Map/init.js
+++ front/components/Map/init.js
@@ -2,8 +2,8 @@ import moment from 'moment'
 
 import Range from '../../models/Range'
 
-export default ({params: {from, to}, location: {action}, accountStartDay, selectRange}) => {
-  if(!from && !to) return selectRange(new Range({start: moment(accountStartDay)}))
+export default ({params: {from, to}, location: {action}, selectRange}) => {
+  if(!from && !to) return selectRange(new Range({start: moment().subtract(1, 'd')}))
   if(action !== 'POP') return
 
   if(to) return selectRange(new Range({start: moment(from, 'YYYYMMDD'), end: moment(to, 'YYYYMMDD')}), false)
--- front/components/MapContainer/index.js
+++ front/components/MapContainer/index.js
@@ -6,7 +6,7 @@ import goTomorrowAction from '../../store/actions/goTomorrow'
 
 import Map from '../Map'
 
-const mapStateToProps = ({map: {selected, loading}, user: {homeLocation, accountStartDay}}) => ({loading, selected, homeLocation, accountStartDay})
+const mapStateToProps = ({map: {selected, loading}, user: {homeLocation}}) => ({loading, selected, homeLocation})
 const mapDispatchToProps = {
   selectRange: (...args) => (dispatch, getState) => selectRangeAction(dispatch, getState, ...args),
   goYesterday: () => (dispatch, getState) => goYesterdayAction(dispatch, getState),
