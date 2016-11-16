dcb406ac30115120a3621aa554562bad50c88670
reuben <reuben.eggar@me.com>
2016-11-16 16:11:47 +0000

    Removed Unnecessary Data

--- front/helpers/protect.js
+++ front/helpers/protect.js
@@ -4,7 +4,7 @@ const protect = ({getState}, isNotAuthPath=null, isAuthPath=null) =>
   (nextState, replace) => {
     const isAuth = getState().user.loggedIn
     const isNotAuthFunc = isNotAuthPath ? () => replace({pathname: isNotAuthPath, state: {nextPathname: nextState.location.pathname}}) : () => null
-    const isAuthFunc = isAuthPath ? () => replace({pathname: isAuthPath, state: {nextPathname: nextState.location.pathname}}) : () => null
+    const isAuthFunc = isAuthPath ? () => replace({pathname: isAuthPath}) : () => null
 
     if(isAuth) isAuthFunc()
     else isNotAuthFunc()
