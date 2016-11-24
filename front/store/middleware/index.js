e73e9e9928dad440b20496da8846468f715c9cca
reuben <reuben.eggar@me.com>
2016-11-24 16:29:33 +0000

    :art: Better Middleware Def

--- front/store/middleware/index.js
+++ front/store/middleware/index.js
@@ -4,7 +4,9 @@ import {routerMiddleware} from 'react-router-redux'
 import thunk from 'redux-thunk'
 
 export default compose(
-  applyMiddleware(thunk),
-  applyMiddleware(routerMiddleware(browserHistory)),
+  applyMiddleware(
+    thunk,
+    routerMiddleware(browserHistory)
+  ),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 )
