2692e06fdcf725f10b8a0ee64a483e00e6c359b1
reuben <reuben.eggar@me.com>
2016-11-24 17:16:26 +0000

    🐛

--- front/components/Map/init.js
+++ front/components/Map/init.js
@@ -2,9 +2,8 @@ import moment from 'moment'
 
 import Range from '../../models/Range'
 
-export default ({params: {from, to}, location: {action}, selectRange}) => {
+export default ({params: {from, to}, selectRange}) => {
   if(!from && !to) return selectRange(new Range({start: moment().subtract(1, 'd')}))
-  if(action !== 'POP') return
 
   if(to) return selectRange(new Range({start: moment(from, 'YYYYMMDD'), end: moment(to, 'YYYYMMDD')}), false)
   selectRange(new Range({start: moment(from, 'YYYYMMDD')}), false)
