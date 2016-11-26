19d85c7e705160b9d95d617328e6b67151d4a5ba
reuben <reuben.eggar@me.com>
2016-11-26 12:05:47 +0000

    Edit Markers

--- front/components/Map/EditControl.js
+++ front/components/Map/EditControl.js
@@ -1,9 +1,12 @@
 import {PropTypes} from 'react'
 import L from 'leaflet'
+import {} from 'leaflet-editable'
 import {} from 'leaflet-draw'
 import {} from 'leaflet-draw/dist/leaflet.draw.css'
 
 import {MapControl} from 'react-leaflet'
+
+import mapIcon from './mapIcon'
 
 export default class EditControl extends MapControl {
   static propTypes = {
@@ -18,10 +21,17 @@ export default class EditControl extends MapControl {
   componentWillMount(){
     console.log(this.context)
     this.leafletElement = new L.Control.Draw(Object.assign({}, {
+      draw: false,
       edit: {
         featureGroup: this.context.layerContainer
       }
     }, this.props))
+
+    L.Edit.Poly = L.Edit.Poly.extend({
+      options: {
+        icon: mapIcon({size: 10})
+      }
+    })
 
     this.context.map.on('draw:created', e => {
       this.context.layerContainer.addLayer(e.layer)
