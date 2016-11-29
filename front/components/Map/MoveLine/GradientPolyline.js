df503c6e5fd5e10af70e1a304bb4f31bd5787715
reuben <reuben.eggar@me.com>
2016-11-29 17:42:56 +0000

    GradientPolyline Take Any Component

--- front/components/Map/MoveLine/GradientPolyline.js
+++ front/components/Map/MoveLine/GradientPolyline.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import {Polyline} from 'react-leaflet'
 
 import Gradient from './Gradient'
 
-const GradientPolyline = props =>
+const GradientPolyline = ({Component, ...props}) =>
   <span style={{display: 'none'}}>
-    <Polyline color={`url("#${props.id}")`} {...props} />
+    <Component color={`url("#${props.id}")`} {...props} />
     <Gradient stops={props.gradient} id={props.id} coords={[props.positions[0], props.positions[props.positions.length-1]]} />
   </span>
 
