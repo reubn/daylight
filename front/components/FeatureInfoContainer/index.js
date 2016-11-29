4dc9e9ad065ae61b6078774d06f9535849a566bf
reuben <reuben.eggar@me.com>
2016-11-29 17:44:43 +0000

    Editing FeatureInfo Action and Status

--- front/components/FeatureInfoContainer/index.js
+++ front/components/FeatureInfoContainer/index.js
@@ -4,9 +4,10 @@ import selectFeatureAction from '../../store/actions/selectFeature'
 
 import FeatureInfo from '../FeatureInfo'
 
-const mapStateToProps = ({map: {selected: {selectedFeature}}, aesthetics: {activityTypes, locationCategories}}) => ({feature: selectedFeature, activityTypes, locationCategories})
+const mapStateToProps = ({map: {selected: {selectedFeature, editingFeature}}, aesthetics: {activityTypes, locationCategories}}) => ({feature: selectedFeature, editing: selectedFeature &&editingFeature && editingFeature.id === selectedFeature.id, activityTypes, locationCategories})
 const mapDispatchToProps = {
-  close: () => (dispatch, getState) => selectFeatureAction(dispatch, getState, null)
+  close: () => (dispatch, getState) => selectFeatureAction(dispatch, getState, null),
+  edit: displayFeature => ({type: 'SELECT_EDIT_FEATURE', displayFeature})
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FeatureInfo)
