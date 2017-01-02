5ed31ad18549d3ee87748aac40728cc81c4adac9
reuben <reuben.eggar@me.com>
2017-01-02 12:21:34 +0000

    :art:

--- front/components/FeatureInfoContainer/FeatureInfo/InfoPane/Move.js
+++ front/components/FeatureInfoContainer/FeatureInfo/InfoPane/Move.js
@@ -2,25 +2,23 @@ import React from 'react'
 
 import {main, field} from './style'
 
-export default ({feature}) => {
-  return (
-    <span className={main}>
-      <span className={field}>
-        <label>Activity</label>
-        <value>{feature.activityName}</value>
-      </span>
-      <span className={field}>
-        <label>Duration</label>
-        <value>{feature.duration}</value>
-      </span>
-      <span className={field}>
-        <label>{'Start -> End'}</label>
-        <value>{feature.startEndDescriptor}</value>
-      </span>
-      <span className={field}>
-        <label>{'Distance'}</label>
-        <value>{feature.displayDistance}</value>
-      </span>
+export default ({feature}) => (
+  <span className={main}>
+    <span className={field}>
+      <label>Activity</label>
+      <value>{feature.activityName}</value>
     </span>
-  )
-}
+    <span className={field}>
+      <label>Duration</label>
+      <value>{feature.duration}</value>
+    </span>
+    <span className={field}>
+      <label>{'Start -> End'}</label>
+      <value>{feature.startEndDescriptor}</value>
+    </span>
+    <span className={field}>
+      <label>{'Distance'}</label>
+      <value>{feature.displayDistance}</value>
+    </span>
+  </span>
+)
